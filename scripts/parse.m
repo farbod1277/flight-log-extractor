be3e213e2724e1a18ee552c7d258205df468fa13
Farbod Torabi <farbod1277@yahoo.com>
2018-11-19 19:34:37 +1100

    fixed bug

--- scripts/parse.m
+++ scripts/parse.m
@@ -2,7 +2,7 @@ function delimitedData = parse(path)
 %Returns a cell array of the raw data delimited by ',' and '\n'
 
 fid = fopen(path);
-rawData = textscan(fid,'%c');
+rawData = fscanf(fid,'%c');
 
 delimitedByLineData = regexp(rawData, '\n', 'split');
 
